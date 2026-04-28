bc700712c5c8abf5674a6a9bce12293c22a606a8
Rybydak <matvei.rybydak@yandex.ru>
2026-04-28 20:15:32 +0300

    импортировали pygame и random сделали константы для размеров поля и сетки

---
+++ the_snake.py
@@ -0,0 +1,9 @@
+from random import choice, randint
+
+import pygame
+
+# Константы для размеров поля и сетки:
+SCREEN_WIDTH, SCREEN_HEIGHT = 640, 480
+GRID_SIZE = 20
+GRID_WIDTH = SCREEN_WIDTH // GRID_SIZE
+GRID_HEIGHT = SCREEN_HEIGHT // GRID_SIZE
