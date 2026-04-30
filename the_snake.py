e19ff4ccaf6baebd28c09b3ecd3ec83bf96642b8
Rybydak <matvei.rybydak@yandex.ru>
2026-04-30 10:32:14 +0300

    еще раз пробую наладить pep8

--- the_snake.py
+++ the_snake.py
@@ -2,32 +2,26 @@ from random import randint
 
 import pygame
 
-
 # Константы для размеров поля и сетки:
 SCREEN_WIDTH, SCREEN_HEIGHT = 640, 480
 GRID_SIZE = 20
 GRID_WIDTH = SCREEN_WIDTH // GRID_SIZE
 GRID_HEIGHT = SCREEN_HEIGHT // GRID_SIZE
-
 # Направления движения:
 UP = (0, -1)
 DOWN = (0, 1)
 LEFT = (-1, 0)
 RIGHT = (1, 0)
-
 # Цвет фона - черный:
 BOARD_BACKGROUND_COLOR = (0, 0, 0)
-
 # Цвет границы ячейки
 BORDER_COLOR = (93, 216, 228)
 # Цвет яблока
 APPLE_COLOR = (255, 0, 0)
 # Цвет змейки
 SNAKE_COLOR = (0, 255, 0)
-
 # Скорость движения змейки:
 SPEED = 20
-
 # Настройка времени:
 clock = pygame.time.Clock()
 
